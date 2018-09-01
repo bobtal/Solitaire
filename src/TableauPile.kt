b237d3b00115d8e6595df14f11c74669a300d8d2
bobtal <talevski.boban@gmail.com>
2018-09-02 01:40:29 +0200

    End of Creating the Tableau video

---
+++ src/TableauPile.kt
@@ -0,0 +1,16 @@
+class TableauPile(var cards: MutableList<Card>) {
+    init {
+        cards.last().faceUp = true
+    }
+
+    fun addCard(newCards: MutableList<Card>): Boolean {
+        if (newCards.first().value == cards.last().value - 1 &&
+                suitCheck(newCards.first(), cards.last())) {
+            
+        }
+    }
+
+    private fun suitCheck(c1: Card, c2: Card): Boolean {
+
+    }
+}
